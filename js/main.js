99b93894a66e1940d67ad12b745053c56f7aba8c
hoonlocal <hoonlocal@naver.com>
2021-02-05 17:29:52 +0900

    21/02/05 커밋

--- js/main.js
+++ js/main.js
@@ -110,3 +110,102 @@ jQuery(function ($) {
 		$(target).toggleClass(txt);
 	});
 });
+
+// 체크박스 전체선택, 전체해제
+var as = document.querySelectorAll(".allSelect");
+var chk = document.querySelectorAll(".js-chk");
+
+function selectToggle(){
+	for(var i=0; i<as.length; i++){
+		if(as[i].classList.contains("active")){
+			as[i].classList.remove("active")
+		}
+	}
+	this.classList.add("active");
+	if(as[0].classList.contains("active")){
+		for(var j=0; j<chk.length; j++){
+			chk[j].checked=true;
+		}
+	} else {
+		for(var k=0; k<chk.length; k++){
+			chk[k].checked=false;
+		}
+	}
+}
+function selectInit(){
+	for(var i=0; i<as.length; i++){
+		as[i].addEventListener("click", selectToggle);
+	}
+}
+
+selectInit();
+
+/* page navigation */
+var pn = document.querySelectorAll(".pn");
+
+function pnToggle(){
+	for(var i=0; i<pn.length; i++){
+		if(pn[i].classList.contains("active")){
+			pn[i].classList.remove("active")
+		}
+	}
+	this.classList.add("active");
+}
+function pnInit(){
+	for(var i=0; i<pn.length; i++){
+		pn[i].addEventListener("click", pnToggle);
+	}
+}
+
+pnInit();
+
+/* tab */
+var ti = document.querySelectorAll(".tab-item");
+
+function tiToggle(){
+	for(var i=0; i<ti.length; i++){
+		if(ti[i].classList.contains("active")){
+			ti[i].classList.remove("active")
+		}
+	}
+	this.classList.add("active");
+}
+function tiInit(){
+	for(var i=0; i<ti.length; i++){
+		ti[i].addEventListener("click", tiToggle);
+	}
+}
+
+tiInit();
+
+/* modal */
+var allM = document.querySelector("#selectMessage");
+var selectM = document.querySelector("#allMessage");
+var myModal = document.querySelector("#myModal");
+var closeBtn = document.querySelector("#closeBtn");
+var overlay = document.querySelector("#overlay");
+
+function startModal(){
+	allM.addEventListener("click", function(){
+		myModal.style.visibility="visible"
+		myModal.style.opacity="1";
+	})
+	selectM.addEventListener("click", function(){
+		myModal.style.visibility="visible"
+		myModal.style.opacity="1";
+	})
+}
+
+function closeModal(){
+	closeBtn.addEventListener("click", function(){
+		myModal.style.visibility="hidden";
+		myModal.style.opacity="0";
+	})
+	overlay.addEventListener("click", function(){
+		myModal.style.visibility="hidden";
+		myModal.style.opacity="0";
+	})
+}
+
+startModal();
+closeModal();
