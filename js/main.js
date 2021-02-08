eb6d1f4b7d30c37e237f6ba1146e2bd08206d002
hoonlocal <hoonlocal@naver.com>
2021-02-08 17:38:15 +0900

    21/02/08 월요일 최종커밋

--- js/main.js
+++ js/main.js
@@ -1,53 +1,6 @@
-// var title = document.getElementsByClassName("js-title");
-// var acTwo = document.getElementsByClassName("two_text");
-// var i, j;
-
-// function navToggle(){
-//     for (i = 0; i < title.length; i++) {
-//         title[i].addEventListener("click", function() {
-//             for (j=0; j<title.length; j++) {
-//                 title[j].classList.remove("active_nav");
-//                 title[j].nextElementSibling.style.display="none";
-//             }
-//             title[0].nextElementSibling.style.display="block";
-//             this.classList.toggle("active_nav");
-//             panel = this.nextElementSibling;
-//             // panel.style.opacity="1";
-//             // panel.style.visibility="visible";
-//             // panel.style.height="100%";
-//             panel.style.display="block";
-//         });
-//     }
-// }
-
-// function activeTwo(){
-//     for(var k=0; k<acTwo.length; k++){
-//         acTwo[k].classList.remove("active_two-nav");
-//     }
-//     this.classList.add("active_two-nav");
-// }
-
-// function activeTwoInit(){
-//     for(var k=0; k<acTwo.length; k++){
-//         acTwo[k].addEventListener("click", activeTwo);
-//     }
-// }
-
-// function init(){
-//     navToggle();
-//     activeTwoInit();
-// }
-
-// init();
-
-
-
-
-
 jQuery(function ($) {
 	var $body = $('body'),
 		$window = $(window);
-	
 	
 	var lastScrollTop = 0,
 		$scrollElement = $('html, body');
@@ -60,21 +13,6 @@ jQuery(function ($) {
 		}
 		lastScrollTop = sT;	
 	}).trigger('scroll');
-	
-//	$('.gnb>li>a').click(function(){
-//		if($(this).parent().find('.dep2').length){
-//			$(this).addClass('on').parent().siblings().find('a').removeClass('on');
-//			$(this).next().slideDown().addClass('on').parent().siblings().find('.dep2').slideUp().removeClass('on');
-//			return false;
-//		};
-//	});
-//	$('.gnb .dep2>li>a').click(function(){
-//		if($(this).parent().find('.dep3').length){
-//			$(this).addClass('on').parent().siblings().find('a').removeClass('on');
-//			$(this).next().slideDown().addClass('on').parent().siblings().find('.dep3').slideUp().removeClass('on');
-//			return false;
-//		};
-//	});
 	$('.gnb>li>a').click(function(){
 		if($(this).parent().find('.dep2').length){
 			$(this).toggleClass('on');
@@ -82,19 +20,6 @@ jQuery(function ($) {
 			return false;
 		};
 	});
-	// $('.gnb .dep2>li>a').click(function(){
-	// 	if($(this).parent().find('.dep3').length){
-	// 		$(this).toggleClass('on');
-	// 		$(this).next().slideToggle(300, 'linear');
-	// 		return false;
-	// 	};
-	// });
-	
-	// $('.gnb .dep2>li>a').addClass(function(){
-	// 	if($(this).parent().find('.dep3').length){
-	// 		$(this).addClass('a-dep3');
-	// 	};
-	// });
 	$('.collapse').on('shown.bs.collapse', function () {
 		$('.btn-collapse[data-target="'+$(this).attr('data-tg')+'"]').find('.txt').text('접기');
 	});
